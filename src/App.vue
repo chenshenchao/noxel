60c586abaa661a71aa35a8c3ce1ce349ef56c4f9
chenshenchao <chenshenchao@outlook.com>
2020-04-06 00:23:13 +0800

    整理配置。

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
         <code v-text="'<el-button>'"></code>
         below
       </p>
-      <el-button>el-button</el-button>
+      <el-button>{{version}}</el-button>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import { Component, Prop, Provide } from "vue-property-decorator";
 
-export default {
-  name: 'app',
+@Component()
+class App extends Vue {
+  @Provide()
+  app = this;
+
+  @Prop({ default: 1 })
+  version;
+
+  created() {
+    console.log(this.app);
+  }
 }
+
+export default App;
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './routes';
-import store from './stores';
+import router from './router';
 import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
